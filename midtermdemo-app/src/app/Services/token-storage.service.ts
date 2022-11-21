bb01055e6f003ba559d0f0beb7b23aad676464d7
Rebecca White <113621309+RWhiteHCL@users.noreply.github.com>
2022-11-20 22:59:36 -0500

    Added blue color theme

--- midtermdemo-app/src/app/Services/token-storage.service.ts
+++ midtermdemo-app/src/app/Services/token-storage.service.ts
@@ -31,6 +31,7 @@ export class TokenStorageService {
     const user = window.sessionStorage.getItem(USER_KEY);
     if (user) {
       return JSON.parse(user);
+      console.log(user);
     }
 
     return {};
